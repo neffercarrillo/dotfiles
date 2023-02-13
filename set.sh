561d2a81330b587f321d8aae528eb73f612f1e57
ncd <ncd@debian-BULLSEYE-live-builder-AMD64>
2023-02-12 22:00:29 -0800

    update folder structure

--- set.sh
+++ set.sh
@@ -2,17 +2,15 @@
 
 SCRIPT_NAME="set.sh"
 
-for i in $( ls ); do
-    if [ $i != "set.sh" ]; then
-		if [ -a ~/.$i ]; then
-			echo $SCRIPT_NAME": deleting file" ~/.$i
-			rm -f ~/.$i
-			echo $SCRIPT_NAME": creating link to ."$i "from "$(pwd)/$i
-			ln -s $(pwd)/$i ~/.$i
-		else
-			echo $SCRIPT_NAME": creating link to ."$i "from "$(pwd)/$i
-			ln -s $(pwd)/$i ~/.$i			
-		fi
-	fi
+for i in $( ls src/); do
+    if [ -a ~/.$i ]; then
+	echo $SCRIPT_NAME": deleting file" ~/.$i
+	rm -f ~/.$i
+	echo $SCRIPT_NAME": creating link to ."$i "from "$(pwd)/src/$i
+	ln -s $(pwd)/src/$i ~/.$i
+    else
+	echo $SCRIPT_NAME": creating link to ."$i "from "$(pwd)/src/$i
+	ln -s $(pwd)/src/$i ~/.$i			
+    fi
 done
         
