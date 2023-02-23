c89630f8fd6cd6c3593c1241720f949e10ce2ea3
Neffer Carrillo <neffer@neffercarrillo.com>
2023-02-22 21:39:01 -0800

    multiple updates

--- set.sh
+++ set.sh
@@ -2,8 +2,8 @@
 
 SCRIPT_NAME="set.sh"
 
-for i in $( ls src/); do
-    if [ -a ~/.$i ]; then
+for i in $(ls src/); do
+    if [ -h ~/.$i ]; then
 	echo $SCRIPT_NAME": deleting file" ~/.$i
 	rm -f ~/.$i
 	echo $SCRIPT_NAME": creating link to ."$i "from "$(pwd)/src/$i
